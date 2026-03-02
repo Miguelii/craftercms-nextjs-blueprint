63943ef63bfb835d767691418db9bb73d6cf3038
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-03-02 23:36:23 +0000

    fix: added missing '()'

--- src/types/experience-builder-override.d.ts
+++ src/types/experience-builder-override.d.ts
@@ -4,6 +4,6 @@ declare module '@craftercms/experience-builder/react' {
     /** @deprecated Import from '@/components/craftercms/render-field' instead to allow Server-Side-Rendering (SSR) */
     export const RenderField: never
 
-    /** @deprecated Import from '@/components/craftercms/experience-builder' instead to allow Server-Side-Rendering SSR */
+    /** @deprecated Import from '@/components/craftercms/experience-builder' instead to allow Server-Side-Rendering (SSR) */
     export const ExperienceBuilder: never
 }
