00d696a70fd9d20f31c2de1404404a4d88b215b6
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-03-05 10:59:46 +0000

    fix: unstable_cache tricky bug (#15)

--- src/components/craftercms/experience-builder.tsx
+++ src/components/craftercms/experience-builder.tsx
@@ -36,6 +36,8 @@ type Props = PropsWithChildren<{
 export function ExperienceBuilder({ model, children }: Props) {
     const { isAuthoring } = use(AuthoringContext)
 
+    console.log({ model })
+
     // Just a double safety check to avoid rendering the ExperienceBuilder when model is null,
     // which would cause it to throw an error.
     if (!model) return children
