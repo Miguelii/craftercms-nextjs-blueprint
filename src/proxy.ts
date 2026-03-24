8fd1f640b128ba97f96bc57dc63ce60619fb89b3
Miguel Gonçalves <miguelgoncalves18@hotmail.com>
2026-03-24 00:35:17 +0000

    feat: added CSP

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { setProxyCrafterCookies } from '@/lib/utils.server'
+import { setCSP } from '@/lib/setCSP'
 
 export function proxy(request: NextRequest) {
     const requestHeaders = new Headers(request.headers)
@@ -10,6 +11,8 @@ export function proxy(request: NextRequest) {
             headers: requestHeaders,
         },
     })
+
+    setCSP(response)
 
     setProxyCrafterCookies(request, response)
 
